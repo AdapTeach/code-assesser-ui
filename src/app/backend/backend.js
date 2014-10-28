0c1b69eaa6a3ddee2268b028a1a4dff4d92ddba6
charl <charles.jacquin@autistici.org>
2014-10-28 05:18:50 +0100

    angry cat

--- src/app/backend/backend.js
+++ src/app/backend/backend.js
@@ -1,4 +1,4 @@
 angular.module('backend', [])
 
-    .constant('BACKEND_URL', 'http://localhost:5010/assess/');
-    //.constant('BACKEND_URL', 'http://codeassesser-charl.rhcloud.com/assess/');
+    //.constant('BACKEND_URL', 'http://localhost:5010/assess/');
+    .constant('BACKEND_URL', 'http://codeassesser-charl.rhcloud.com/assess/');
